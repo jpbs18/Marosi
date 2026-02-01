9357aeee77f337bf2c81922f073404e83ac032b2
Joao <j.p.santos21@hotmail.com>
2026-02-01 12:11:55 +0000

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,13 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Marosi | Construção, Renovação e Fix&Flip Imobiliário",
   description: "Construção, Renovação e Fix&Flip Imobiliário",
+  icons: {
+    icon: [
+      `${process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL}/w_16,h_16,c_fill,f_png/grwotcskzda02qxhnzeu.png`,
+      `${process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL}/w_32,h_32,c_fill,f_png/grwotcskzda02qxhnzeu.png`,
+    ],
+    apple: `${process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL}/w_180,h_180,c_fill,f_png/grwotcskzda02qxhnzeu.png`,
+  },
 };
 
 export default function RootLayout({
@@ -11,17 +18,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const favicon32 = `${process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL}/w_32,h_32,c_fill,f_auto/grwotcskzda02qxhnzeu.webp`;
-  const favicon16 = `${process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL}/w_16,h_16,c_fill,f_auto/grwotcskzda02qxhnzeu.webp`;
-  const appleIcon = `${process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL}/w_180,h_180,c_fill,f_auto/grwotcskzda02qxhnzeu.webp`;
-
   return (
     <html lang="pt">
-      <head>
-        <link rel="icon" type="image/png" href={favicon16} />
-        <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
-        <link rel="apple-touch-icon" href={appleIcon} />
-      </head>
       <body className="min-h-screen flex flex-col">{children}</body>
     </html>
   );
